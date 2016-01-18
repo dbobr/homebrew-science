306006a5b82f8c4901fe4d315dd5f8f536295ddc
Dmitriy Bobrovnikov <dmitriy.bobrovnikov@gmail.com>
2016-01-18 13:20:07 -0500

    Update phyml.rb

--- phyml.rb
+++ phyml.rb
@@ -1,17 +1,17 @@
 class Phyml < Formula
   desc "Fast maximum likelihood-based phylogenetic inference"
+  bottle do
+    cellar :any
+    sha256 "ccae08800290289e8989d6da5aab65adb6cded93e6a32b284f184a7c372f947b" => :yosemite
+    sha256 "c8cc4b9434128305c7a759a73e67f28dc3a4418531be80694ab80a06786e770d" => :mavericks
+    sha256 "723aa3d01acf9ec3b4fc4aa8ed48abd0db5208abb9c7f8e23c5549e6668a29b5" => :mountain_lion
+  end
+
+  # tag "bioinformatics"
+  # doi "10.1093/sysbio/syq010"
   homepage "http://www.atgc-montpellier.fr/phyml/"
   url "https://github.com/stephaneguindon/phyml/archive/v3.2.0.tar.gz"
   sha256 "9fec8fc26e69cad8d58bf2c9433b531754e4f026dc3464d07958b6c824783fde"
-  # tag "bioinformatics"
-  # doi "10.1093/sysbio/syq010"
-
-  bottle do
-    cellar :any_skip_relocation
-    sha256 "54afc7cc2ff1bfbe3ee89b60431132e4ab57d0ff9091677846ae3c306603d55d" => :el_capitan
-    sha256 "10f0ed39855d390490c069e16da43ec98b106acde013201fb9e757bb25f3ec8d" => :yosemite
-    sha256 "dac19c8053a821f6a28057cd67863733b48c3372309f1d8bacfa904e2da5701e" => :mavericks
-  end
 
   depends_on "autoconf" => :build
   depends_on "automake" => :build
@@ -34,10 +34,8 @@ class Phyml < Formula
   def caveats; <<-EOS.undent
     Examples have been installed here:
       #{opt_pkgshare}
-
     See options for phyml by running:
       phmyl --help
-
     PhyML must be run with the "-i" option to specify an input or it will
     segfault. Example:
       phyml -i #{opt_pkgshare}/nucleic
